cdbf2ea71163b4b670898bcabd77a6208dcdbb18
Omarbojo <obojorquez@mail.csuchico.edu>
2018-09-19 16:25:28 -0700

    Asks for names

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Omar Bojorquez
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+  string firstname = "";
+  string lastname = "";
 
   //get user input
+  std::cout<<"What is your firstname?"<<std::endl;
+  std::cin>>firstname;
+  std::cout<<"What is your lastname?"<<std::endl;
+  std::cin>>lastname;
 
   //tell fortune
 
